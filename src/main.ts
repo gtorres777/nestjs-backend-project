fe751cffa76daa97406684a1984802e75ad91f87
biotux <tuxknrp@gmail.com>
2020-11-06 01:01:21 -0500

    swagger agregado

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { initSwagger } from './app.swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  initSwagger(app);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }))
